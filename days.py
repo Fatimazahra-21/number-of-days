f7e1206faaaf6d3de688ec1ae200432af0433642
Fatima zahra majdou <146893864+Fatimazahra-21@users.noreply.github.com>
2023-10-09 04:47:19 +0300

    Create days.py

---
+++ days.py
@@ -0,0 +1,31 @@
+M = input("entrer le mois":)
+if M = 1 :
+print("31")
+elif M = 2 :
+print("28 or 29")
+elif M = 3 :
+print("31")
+elif M = 4 :
+print("30")
+elif M = 5 :
+print("31")
+elif M = 6 :
+print("30")
+elif M = 7 :
+print("31")
+elif M = 8 :
+print("31")
+elif M = 9 :
+print("30")
+elif M = 10 :
+print("31")
+elif M = 11 :
+print("30")
+elif M = 12 :
+print("31")
+else:
+print("le mois nexiste pas")
+
+
+
+
